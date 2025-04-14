5bd29e074df3a6cf343e5767d087572d273de986
smini <sm020320@naver.com>
2025-04-14 14:51:23 +0900

    init: initial project setup

---
+++ Idam/src/main/java/com/team7/Idam/IdamApplication.java
@@ -0,0 +1,13 @@
+package com.team7.Idam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdamApplication.class, args);
+	}
+
+}
---
+++ Idam/src/test/java/com/team7/Idam/IdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.Idam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
