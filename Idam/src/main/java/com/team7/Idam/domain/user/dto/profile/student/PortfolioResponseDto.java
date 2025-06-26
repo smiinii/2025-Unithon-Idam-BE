6a6ddb5e100a39b97f390eb1964614e85b1d55e5
yim0327 <yim0327@naver.com>
2025-06-26 23:11:16 +0900

    refactor: File/Url 포트폴리오 추가 API 분기

--- Idam/src/main/java/com/team7/Idam/domain/user/dto/profile/student/PortfolioResponseDto.java
+++ Idam/src/main/java/com/team7/Idam/domain/user/dto/profile/student/PortfolioResponseDto.java
@@ -1,5 +1,6 @@
 package com.team7.Idam.domain.user.dto.profile.student;
 
+import com.team7.Idam.domain.user.entity.Portfolio;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,4 +9,15 @@ import lombok.Data;
 public class PortfolioResponseDto {
     private Long id;
     private String url;
+    private String type; // "File" 또는 "URL"
+
+    public static PortfolioResponseDto from(Portfolio p) {
+        String value = p.getPortfolio();
+        String type = isS3FileUrl(value) ? "FILE" : "URL";
+        return new PortfolioResponseDto(p.getPortfolioId(), value, type);
+    }
+
+    private static boolean isS3FileUrl(String value) {
+        return value != null && value.startsWith("https://unithon-idam.s3.");
+    }
 }
